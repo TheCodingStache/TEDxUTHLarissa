410fd8af87a9ac9070a07c49de6c2ca2429f2c55
TheCodingStache <cs4415161@teilar.gr>
2020-02-15 22:35:10 +0200

    Navigation Drawer is on its way

--- app/src/main/java/thecodingstache/tedxuthlarissa/MainActivity.java
+++ app/src/main/java/thecodingstache/tedxuthlarissa/MainActivity.java
@@ -1,11 +1,16 @@
 package thecodingstache.tedxuthlarissa;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
@@ -20,7 +25,6 @@ import com.google.android.material.navigation.NavigationView;
 import com.shrikanthravi.customnavigationdrawer2.widget.SNavigationDrawer;
 
 import thecodingstache.tedxuthlarissa.Fragment.MapFragment;
-import thecodingstache.tedxuthlarissa.Fragment.MenuFragment;
 import thecodingstache.tedxuthlarissa.Fragment.ProfileFragment;
 import thecodingstache.tedxuthlarissa.Fragment.ScheduleFragment;
 import thecodingstache.tedxuthlarissa.Fragment.SpeakersFragment;
@@ -29,14 +33,17 @@ import thecodingstache.tedxuthlarissa.Fragment.TeamFragment;
 public class MainActivity extends AppCompatActivity {
     SNavigationDrawer sNavigationDrawer;
     Class fragmentClass;
+    private boolean mToolBarNavigationListenerIsRegistered = false;
     public static Fragment mFragment;
     private ImageView imageView;
     private TextView mTextView;
     NavController navController;
     DrawerLayout drawer;
+    ActionBarDrawerToggle mActionBarDrawerToggle;
     NavigationView navigationView;
     AppBarConfiguration appBarConfiguration;
     BottomNavigationView bottomNavigationView;
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,31 +56,34 @@ public class MainActivity extends AppCompatActivity {
         navigationView = findViewById(R.id.navigationView);
         navController = Navigation.findNavController(this, R.id.main_fragment);
         bottomNavigationView = findViewById(R.id.bottom_navigation);
-        appBarConfiguration = new AppBarConfiguration.Builder(new int[]{R.id.main, R.id.secondary, R.id.third, R.id.forth})
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, mToolbar, R.string.nav_app_bar_open_drawer_description, R.string.nav_app_bar_navigate_up_description);
+        drawer.addDrawerListener(mActionBarDrawerToggle);
+        mActionBarDrawerToggle.syncState();
+        appBarConfiguration = new AppBarConfiguration.Builder()
                 .setDrawerLayout(drawer)
                 .build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-//        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
 //        sNavigationDrawer = findViewById(R.id.navigationDrawer);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
 
-//        fragmentClass = MenuFragment.class;
-//        try {
-//            mFragment = (Fragment) fragmentClass.newInstance();
-//        } catch (IllegalAccessException | InstantiationException e) {
-//            e.printStackTrace();
-//        }
-//        if (mFragment != null) {
-//            FragmentManager fragmentManager = getSupportFragmentManager();
-//            fragmentManager.beginTransaction()
-//                    .setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
-//                    .replace(R.id.frameLayout, mFragment).commit();
-//        }
+        fragmentClass = ScheduleFragment.class;
+        try {
+            mFragment = (Fragment) fragmentClass.newInstance();
+        } catch (IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
+        if (mFragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
+                    .replace(R.id.frameLayout, mFragment).commit();
+        }
 
     }
 
@@ -121,5 +131,62 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, appBarConfiguration);
     }
 
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void enableViews(boolean enable) {
+
+        // To keep states of ActionBar and ActionBarDrawerToggle synchronized,
+        // when you enable on one, you disable on the other.
+        // And as you may notice, the order for this operation is disable first, then enable - VERY VERY IMPORTANT.
+        if (enable) {
+            //You may not want to open the drawer on swipe from the left in this case
+            drawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+            // Remove hamburger
+            mActionBarDrawerToggle.setDrawerIndicatorEnabled(false);
+            // Show back button
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            // when DrawerToggle is disabled i.e. setDrawerIndicatorEnabled(false), navigation icon
+            // clicks are disabled i.e. the UP button will not work.
+            // We need to add a listener, as in below, so DrawerToggle will forward
+            // click events to this listener.
+            if (!mToolBarNavigationListenerIsRegistered) {
+                mActionBarDrawerToggle.setToolbarNavigationClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Doesn't have to be onBackPressed
+                        onBackPressed();
+                    }
+                });
+
+                mToolBarNavigationListenerIsRegistered = true;
+            }
+
+        } else {
+            //You must regain the power of swipe for the drawer.
+            drawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+
+            // Remove back button
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            // Show hamburger
+            mActionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+            // Remove the/any drawer toggle listener
+            mActionBarDrawerToggle.setToolbarNavigationClickListener(null);
+            mToolBarNavigationListenerIsRegistered = false;
+        }
+
+        // So, one may think "Hmm why not simplify to:
+        // .....
+        // getSupportActionBar().setDisplayHomeAsUpEnabled(enable);
+        // mDrawer.setDrawerIndicatorEnabled(!enable);
+        // ......
+        // To re-iterate, the order in which you enable and disable views IS important #dontSimplify.
+    }
 }
 
